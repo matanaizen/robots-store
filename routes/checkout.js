f56d5dd5693412c9f6680c40acb72369a10ec6a4
shirabarkai <shira123barkai@gmail.com>
2024-08-29 17:15:00 +0300

    Checkout logic

--- routes/checkout.js
+++ routes/checkout.js
@@ -9,7 +9,33 @@ router.post("/checkout", async (req, res) => {
   if (cart.length === 0) {
     return res.status(400).send("Cart is empty");
   }
+
+  try {
+    let user = req.session.user;
+    const userById = await User.findById(user._id);
+
+    if (!userById) {
+      user = await User.findOne({ email: user.email });
+    }
+
+    const order = new Order({
+      user: user._id,
+      items: cart,
+      totalAmount: Number(
+        cart.reduce(
+          (total, item) => total + item.quantity * item.product.price,
+          0
+        )
+      ).toFixed(2),
+    });
+
+    await order.save();
+    req.session.cart = [];
+
     res.status(200).send("Order completed");
+  } catch (error) {
+    res.status(500).send("Server error");
+  }
 });
 
 module.exports = router;
